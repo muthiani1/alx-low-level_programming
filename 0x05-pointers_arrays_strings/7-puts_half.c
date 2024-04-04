7152a9cf0358c13fa9dc55cca5f4aa39fb1114ff
Muthiani <bluntcoder254@gmail.com>
2024-04-04 11:09:03 +0300

    5.9

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,12 +13,10 @@ void puts_half(char *str)
 
 	while (str[len] != '\0')
 		len++;
-	printf("\nlen: %d", len);
 	if (len > 0 && len % 2)
 		n = (len + 1) / 2;
 	else
 		n = len / 2;
-	printf("\nn: %d, str[n]: %c", n, str[n]); 
 	while (n != 0 && str[n] != '\0')
 	{
 		_putchar(str[n]);
