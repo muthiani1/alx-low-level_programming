f495cd01a99740d5f5e2e9e1e40028a999184d70
Muthiani <bluntcoder254@gmail.com>
2024-04-05 11:14:25 +0300

    6.4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i;
 
-	i = j = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	if (i > j)
-		return (15);
-	else if (i < j)
-		return (-15);
-	else
-		return (0);
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (s1[i] - s2[i]);
 }
