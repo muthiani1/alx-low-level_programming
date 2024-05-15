f8bf262b20a45abed90ccb5c5420acd4ff90a4a1
Muthiani <bluntcoder254@gmail.com>
2024-05-15 10:19:43 +0300

    5.10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,38 +1,46 @@
 #include "main.h"
 
 /**
- * _atoi - converts string to integer
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * @s: sring
- * Return: Converted integer
+ * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
-	int i, num, sign;
+	int i, d, n, len, f, digit;
 
-	i = num = sign = 0;
-	while (s[i] != '\0')
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		if (s[i] == '+')
-			sign++;
-		else if (s[i] == '-')
-			sign--;
-		else if (s[i] >= 48 && s[i] <= 57)
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			num = s[i++] - 48;
-			while (s[i] >= 48 && s[i] <= 57)
-			{
-				num = (num * 10) + (s[i] - 48);
-				i++;
-			}
-			break;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
 		i++;
 	}
 
-	if (sign < 0)
-		return (num * -1);
-	else
-		return (num);
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
