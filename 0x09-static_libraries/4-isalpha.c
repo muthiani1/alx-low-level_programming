481843a3309ff4f3cc4d67684e91e04226820b60
Muthiani <bluntcoder254@gmail.com>
2024-05-20 11:51:50 +0300

    9

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,3 +1,10 @@
+/**
+ * _isalpha - checks if a character is an alphabet
+ *
+ * @c: character
+ * Return: 1 (true) 0 (false)
+ */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
