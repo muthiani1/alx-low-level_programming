a1f6b09daae7ac1242308b43b83071091a80b9df
Muthiani <bluntcoder254@gmail.com>
2024-07-08 15:34:38 +0300

    B.101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,22 +1,17 @@
 #include "main.h"
 
 /**
- * strtow - splits a string into words
+ * count_words - counts number of words in a string
  *
  * @str: string
- * Return: an array of seperated words
+ * Return: number of words
  */
 
-char **strtow(char *str)
+int count_words(char *str)
 {
-	int i, j;	/* loop controllers */
-	int num_words, word_index, word_start, word_length;
+	int i, count = 0;
 	int inWord = 0;
-	char **words;
 
-	num_words = 0;
-	if (str == NULL || *str == '\0')
-		return NULL;
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == ' ')
@@ -25,35 +20,44 @@ char **strtow(char *str)
 		}
 		else if (!inWord)
 		{
-			num_words++;
+			count++;
 			inWord = 1;
 		}
 	}
+	return (count);
+}
 
+/**
+ * strtow - splits a string into words
+ *
+ * @str: string
+ * Return: an array of seperated words
+ */
+
+char **strtow(char *str)
+{
+	int i, j, num_words, word_index, word_start, word_length, inWord = 0;
+	char **words;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	num_words = count_words(str);
 	words = malloc((num_words + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
-
 	word_index = word_start = word_length = 0;
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ')
+		if (str[i] == ' ' || str[i + 1] == '\0')
 		{
 			if (inWord)
 			{
 				words[word_index] = malloc((word_length + 1) * sizeof(char));
 				if (words[word_index] == NULL)
-				{
-					for (j = 0; j < word_length; j++)
-						free(words[i]);
-					free(words);
 					return (NULL);
-				}
-
 				for (j = 0; j < word_length; j++)
 					words[word_index][j] = str[word_start + j];
 				words[word_index][word_length] = '\0';
-
 				word_index++;
 			}
 			inWord = 0;
@@ -68,23 +72,6 @@ char **strtow(char *str)
 			}
 			word_length++;
 		}
-	}
-	if (word_length > 0)
-	{
-		words[word_index] = malloc((word_length + 1) * sizeof(char));
-		if (words[word_index] == NULL)
-		{
-			for (j = 0; j < word_length; j++)
-				free(words[i]);
-			free(words);
-			return (NULL);
-		}
-
-		for (j = 0; j < word_length; j++)
-			words[word_index][j] = str[word_start + j];
-		words[word_index][word_length] = '\0';
-
-		word_index++;
 	}
 	words[word_index] = NULL;
 	return (words);
